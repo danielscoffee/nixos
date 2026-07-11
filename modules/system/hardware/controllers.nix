06e9499683ea953cbd12aa3d7bdd786f96b7d626
danielscoffee <danielrsantoscontato@gmail.com>
2026-07-11 14:25:27 -0300

    feat(system): enable Xbox controller support

---
+++ modules/system/hardware/controllers.nix
@@ -0,0 +1,7 @@
+{
+  # Xbox controller support:
+  # - xpadneo: Xbox One/Series controllers over Bluetooth
+  # - xone: Xbox One/Series wireless adapter and accessories
+  hardware.xpadneo.enable = true;
+  hardware.xone.enable = true;
+}
